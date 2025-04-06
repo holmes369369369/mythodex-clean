a96fcb6de0ce3dc582eae725e8c2b74805613d50
holmes369369369 <chris.holmes85@hotmail.com>
2025-04-06 01:04:02 +0100

    🚀 Final fix - pushing working index.js to Vercel

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World from Mythodex!</h1>
+      <p>This is your first deployed page with Vercel ⚔️🛡️</p>
+    </div>
+  );
+}
